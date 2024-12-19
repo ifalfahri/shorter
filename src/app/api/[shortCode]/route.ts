5ae04221b6dff1517e0e9a3290ce6ad4231fbd09
IfalEXZ <ifaltampan69@gmail.com>
2024-12-19 21:39:17 +0800

    feat: refactor Prisma client initialization and add route for retrieving original URLs

---
+++ src/app/api/[shortCode]/route.ts
@@ -0,0 +1,24 @@
+import prisma from '../../../../lib/prisma'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+  request: Request,
+  { params }: { params: { shortCode: string } }
+) {
+  const shortCode = params.shortCode
+
+  try {
+    const shortUrl = await prisma.shortUrl.findUnique({
+      where: { shortCode },
+    })
+
+    if (shortUrl) {
+      return NextResponse.redirect(shortUrl.originalUrl)
+    } else {
+      return new NextResponse('Short URL not found', { status: 404 })
+    }
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
+
