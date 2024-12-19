1adfe4e9c1f5ae425d746a7135d41af238219c46
IfalEXZ <ifaltampan69@gmail.com>
2024-12-19 21:10:38 +0800

    feat: enhance Home component layout with centered title and URL form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Shorter URL</h1>
+    <main className="container h-screen mx-auto p-4 pt-20">
+      <h1 className="text-2xl text-center font-bold">Shorter URL</h1>
+      <p className="text-center mb-4">Make your url shorter</p>
+      <form action="" className="mb-4 flex justify-center items-center flex-row gap-2">
+        <input type="url" name="url" placeholder="Enter your URL" required className="w-96 p-2 border rounded" />
+        <button type="submit" className="p-2 bg-blue-500 text-white rounded">Make it shorter!</button>
+      </form>
     </main>
   );
 }
