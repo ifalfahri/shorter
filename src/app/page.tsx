ec7db876813b5f34302c772adf2e7368290ab380
IfalEXZ <ifaltampan69@gmail.com>
2024-12-21 15:42:38 +0800

    feat: integrate ShortUrlDisplay component into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { createShortUrl } from './actions'
+import ShortUrlDisplay from './components/ShortUrlDisplay';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <input type="url" name="url" placeholder="Enter your URL" required className="w-96 p-2 border rounded" />
         <button type="submit" className="p-2 bg-blue-500 text-white rounded">Make it shorter!</button>
       </form>
+      <ShortUrlDisplay />
     </main>
   );
 }
